785d8d6499641f0be157e0197747875c1d546b03
Gh0sT-Tech <shaibusalim0000@gmail.com>
2022-09-24 23:29:13 -0700

    my second commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+   
+function myToggle() {
+    const toggleCLick = document.querySelector('.menu');
+    const sideBar = document.querySelector('.sidebar')
+    toggleCLick.classList.toggle('active');
+    sideBar.classList.toggle('active');
+    
+}
